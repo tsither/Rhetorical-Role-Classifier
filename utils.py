24ed68c408be115d5c62f67746435ec968537f53
Kshitij Kar <kar@uni-potsdam.de>
2024-02-25 20:39:43 +0100

    failed attempts at training

--- utils.py
+++ utils.py
@@ -1,11 +1,11 @@
 import torch
 from torch import TensorType
-from sklearn.preprocessing import LabelBinarizer
+from sklearn.preprocessing import LabelEncoder
 from transformers import BertTokenizer, BertModel
 
-def label_encode(target_variables : list) -> LabelBinarizer:
+def label_encode(target_variables : list) -> LabelEncoder:
     """
-    Encode target variables using one-hot encoding.
+    Encode target variables.
     
     Args:
     - target_variables (list or array-like): List of target variable strings.
@@ -13,28 +13,42 @@ def label_encode(target_variables : list) -> LabelBinarizer:
     Returns:
     - lb (object): class object used to tranform and inverse transform.
     """
-    lb = LabelBinarizer()
-    lb = lb.fit(target_variables)
+    le = LabelEncoder()
+    le = le.fit(target_variables)
     
-    return lb
+    return le
 
 def sent2tensors(sentence: str, MAX_LEN = None) -> dict:
     
-    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case = True)
     # print(tokenizer.tokenize(sentence))
     if MAX_LEN is None:
-        inputs = tokenizer(sentence, return_tensors="pt", truncation=True, padding=True)
+        inputs = tokenizer(sentence, return_tensors="pt", truncation= True, padding= True, add_special_tokens= True)
     else:
-        inputs = tokenizer(sentence, return_tensors="pt", truncation=True, padding='max_length',max_length = MAX_LEN)
+        inputs = tokenizer(sentence, return_tensors="pt",
+                           truncation=True, padding='max_length', 
+                           max_length = MAX_LEN, add_special_tokens= True)
     
     return inputs
 
-def sent2embeddings(sentence: str, MAX_LEN = None) -> TensorType:
-    
+def sent2wordemb(sentence: str, MAX_LEN = None) -> torch.TensorType:
     model = BertModel.from_pretrained('bert-base-uncased')
+    for param in model.parameters():
+        param.requires_grad = False
     
     inputs = sent2tensors(sentence,MAX_LEN)
     with torch.no_grad():
-        emb = model(**inputs).last_hidden_state
+        emb = model(**inputs)
+    
+    return emb[0]
+
+def sent2emb(sentence: str) -> torch.TensorType:
+    model = BertModel.from_pretrained('bert-base-uncased')
+    for param in model.parameters():
+        param.requires_grad = False
+    
+    inputs = sent2tensors(sentence)
+    with torch.no_grad():
+        emb = model(**inputs).pooler_output
     
     return emb
